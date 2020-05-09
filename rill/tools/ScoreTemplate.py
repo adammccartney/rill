9e08d14f6aba5a18ae4958761c2d380d10bd9708
adammccartney <adam@mur.at>
2020-05-09 10:59:21 +0200

    moved score template

--- rill/tools/ScoreTemplate.py
+++ rill/tools/ScoreTemplate.py
@@ -1,78 +1,15 @@
 import abjad
-import mccartney 
-import ins_wasser
+
+"""
+Defines scoretemplate, subclass of abjad.ScoreTemplate. Segments appended later. 
+"""
+
 
 
 class ScoreTemplate(abjad.ScoreTemplate):
-    r"""
-    Score template.
-
-    >>> import ins_wasser
-
-    ..  container:: example
-
-        >>> template = ins_wasser.ScoreTemplate()
-        >>> path = abjad.Path('ins_wasser', 'stylesheets', 'contexts.ily')
-        >>> lilypond_file = template.__illustrate__(
-        ...     global_staff_size=14,
-        ...     includes=[path],
-        ...     )
-        >>> abjad.show(lilypond_file) # doctest: +SKIP
-
-        >>> abjad.f(lilypond_file[abjad.Score], strict=89)
-        \context Score = "Score"                                                                 %! ins_wasser.ScoreTemplate.__call__()
-        <<                                                                                       %! ins_wasser.ScoreTemplate.__call__()
-            \context GlobalContext = "Global_Context"                                            %! abjad.ScoreTemplate._make_global_context()
-            <<                                                                                   %! abjad.ScoreTemplate._make_global_context()
-                \context GlobalRests = "Global_Rests"                                            %! abjad.ScoreTemplate._make_global_context()
-                {                                                                                %! abjad.ScoreTemplate._make_global_context()
-                }                                                                                %! abjad.ScoreTemplate._make_global_context()
-                \context GlobalSkips = "Global_Skips"                                            %! abjad.ScoreTemplate._make_global_context()
-                {                                                                                %! abjad.ScoreTemplate._make_global_context()
-                }                                                                                %! abjad.ScoreTemplate._make_global_context()
-            >>                                                                                   %! abjad.ScoreTemplate._make_global_context()
-            \context MusicContext = "Music_Context"                                              %! ins_wasser.ScoreTemplate.__call__()
-            {                                                                                    %! ins_wasser.ScoreTemplate.__call__()
-                \context PianoStaff = "Piano_Staff"                                              %! ins_wasser.ScoreTemplate.__call__()
-                <<                                                                               %! ins_wasser.ScoreTemplate.__call__()
-                    \context Staff = "Viola_I"                                                   %! ins_wasser.ScoreTemplate.__call__()
-                    <<                                                                           %! ins_wasser.ScoreTemplate.__call__()
-                        \context Voice = "Viola_I_Markup_Voice"                                  %! ins_wasser.ScoreTemplate.__call__()
-                        {                                                                        %! ins_wasser.ScoreTemplate.__call__()
-                            \clef "alto"                                                         %! abjad.ScoreTemplate.attach_defaults(3)
-                            s1                                                                   %! abjad.ScoreTemplate.__illustrate__()
-                        }                                                                        %! ins_wasser.ScoreTemplate.__call__()
-                        \context Voice = "Viola_I_Music_Voice"                                   %! ins_wasser.ScoreTemplate.__call__()
-                        {                                                                        %! ins_wasser.ScoreTemplate.__call__()
-                            s1                                                                   %! abjad.ScoreTemplate.__illustrate__()
-                        }                                                                        %! ins_wasser.ScoreTemplate.__call__()
-                        \context Voice = "Viola_I_Dynamics_Voice"                                %! ins_wasser.ScoreTemplate.__call__()
-                        {                                                                        %! ins_wasser.ScoreTemplate.__call__()
-                            s1                                                                   %! abjad.ScoreTemplate.__illustrate__()
-                        }                                                                        %! ins_wasser.ScoreTemplate.__call__()
-                    >>                                                                           %! ins_wasser.ScoreTemplate.__call__()
-                    \context Staff = "Viola_II"                                                  %! ins_wasser.ScoreTemplate.__call__()
-                    <<                                                                           %! ins_wasser.ScoreTemplate.__call__()
-                        \context Voice = "Viola_II_Markup_Voice"                                 %! ins_wasser.ScoreTemplate.__call__()
-                        {                                                                        %! ins_wasser.ScoreTemplate.__call__()
-                            \clef "alto"                                                         %! abjad.ScoreTemplate.attach_defaults(3)
-                            s1                                                                   %! abjad.ScoreTemplate.__illustrate__()
-                        }                                                                        %! ins_wasser.ScoreTemplate.__call__()
-                        \context Voice = "Viola_II_Music_Voice"                                  %! ins_wasser.ScoreTemplate.__call__()
-                        {                                                                        %! ins_wasser.ScoreTemplate.__call__()
-                            s1                                                                   %! abjad.ScoreTemplate.__illustrate__()
-                        }                                                                        %! ins_wasser.ScoreTemplate.__call__()
-                        \context Voice = "Viola_II_Dynamics_Voice"                               %! ins_wasser.ScoreTemplate.__call__()
-                        {                                                                        %! ins_wasser.ScoreTemplate.__call__()
-                            s1                                                                   %! abjad.ScoreTemplate.__illustrate__()
-                        }                                                                        %! ins_wasser.ScoreTemplate.__call__()
-                    >>                                                                           %! ins_wasser.ScoreTemplate.__call__()
-                >>                                                                               %! ins_wasser.ScoreTemplate.__call__()
-            }                                                                                    %! ins_wasser.ScoreTemplate.__call__()
-        >>                                                                                       %! ins_wasser.ScoreTemplate.__call__()
-
     """
-
+    Score template.
+    """
     ### CLASS VARIABLES ###
 
     __documentation_section__ = None
@@ -84,7 +21,9 @@ class ScoreTemplate(abjad.ScoreTemplate):
     def __init__(self):
         super(ScoreTemplate, self).__init__()
         self.voice_abbreviations.update(
-            {"va1": "Viola_I_Music_Voice", "va2": "Viola_II_Music_Voice"}
+            {"Fl.": "Flute_Music_Voice", "ClBb." : "ClarinetBb_Music_Voice",
+                "Guit.": "Guitar", "Vla.": "Viola"
+                }
         )
 
     ### SPECIAL METHODS ###
@@ -93,55 +32,89 @@ class ScoreTemplate(abjad.ScoreTemplate):
         """
         Calls score template.
         """
-        site = "ins_wasser.ScoreTemplate.__call__()"
+        site = "rill.ScoreTemplate.__call__()"
         tag = abjad.Tag(site)
 
         # GLOBAL CONTEXT
         global_context = self._make_global_context()
 
-        # VIOLA 1
-        markup_voice = abjad.Voice(name="Viola_I_Markup_Voice", tag=tag)
-        music_voice = abjad.Voice(name="Viola_I_Music_Voice", tag=tag)
-        dynamics_voice = abjad.Voice(name="Viola_I_Dynamics_Voice", tag=tag)
-        viola_1_staff = abjad.Staff(
+        # Flute
+        markup_voice = abjad.Voice(name="Flute_Markup_Voice", tag=tag)
+        music_voice = abjad.Voice(name="Flute_Music_Voice", tag=tag)
+        dynamics_voice = abjad.Voice(name="Flute_Dynamics_Voice", tag=tag)
+        flute_staff = abjad.Staff(
             [markup_voice, music_voice, dynamics_voice],
             simultaneous=True,
-            name="Viola_I",
+            name="Flute",
             tag=tag,
         )
         abjad.annotate(
-            viola_1_staff,
+            flute_staff,
             "default_instrument",
-            ins_wasser.instruments["ViolaI"],
+            rill.instruments["Flute"],
         )
-        abjad.annotate(viola_1_staff, "default_clef", abjad.Clef("alto"))
+        abjad.annotate(flute_staff, "default_clef", abjad.Clef("treble"))
 
-        # VIOLA 2
-        markup_voice = abjad.Voice(name="Viola_II_Markup_Voice", tag=tag)
-        music_voice = abjad.Voice(name="Viola_II_Music_Voice", tag=tag)
-        dynamics_voice = abjad.Voice(name="Viola_II_Dynamics_Voice", tag=tag)
-        viola_2_staff = abjad.Staff(
+        # Bb_Clarinet
+        markup_voice = abjad.Voice(name="Bb_Clarinet_Markup_Voice", tag=tag)
+        music_voice = abjad.Voice(name="Bb_Clarinet_Music_Voice", tag=tag)
+        dynamics_voice = abjad.Voice(name="Bb_Clarinet_Dynamics_Voice", tag=tag)
+        bb_clarinet_staff = abjad.Staff(
             [markup_voice, music_voice, dynamics_voice],
             simultaneous=True,
-            name="Viola_II",
+            name="Bb_Clarinet",
             tag=tag,
         )
         abjad.annotate(
-            viola_2_staff,
+            bb_clarinet_staff,
             "default_instrument",
-            ins_wasser.instruments["ViolaII"],
+            rill.instruments["Bb_Clarinet"],
+        )
+        abjad.annotate(bb_clarinet_staff, "default_clef", abjad.Clef("treble"))
+
+        # Guitar 
+        markup_voice = abjad.Voice(name="Guitar_Markup_Voice", tag=tag)
+        music_voice = abjad.Voice(name="Guitar_Music_Voice", tag=tag)
+        dynamics_voice = abjad.Voice(name="Guitar_Dynamics_Voice", tag=tag)
+        guitar_staff = abjad.Staff(
+            [markup_voice, music_voice, dynamics_voice],
+            simultaneous=True,
+            name="Guitar",
+            tag=tag,
+        )
+        abjad.annotate(
+            guitar_staff,
+            "default_instrument",
+            rill.instruments["Guitar"],
+        )
+        abjad.annotate(guitar_staff, "default_clef", abjad.Clef("treble"))
+
+        # VIOLA  
+        markup_voice = abjad.Voice(name="Viola_Markup_Voice", tag=tag)
+        music_voice = abjad.Voice(name="Viola_Music_Voice", tag=tag)
+        dynamics_voice = abjad.Voice(name="Viola_Dynamics_Voice", tag=tag)
+        viola_staff = abjad.Staff(
+            [markup_voice, music_voice, dynamics_voice],
+            simultaneous=True,
+            name="Viola",
+            tag=tag,
+        )
+        abjad.annotate(
+            viola_staff,
+            "default_instrument",
+            rill.instruments["Viola"],
         )
         abjad.annotate(viola_2_staff, "default_clef", abjad.Clef("alto"))
 
         # SCORE
-        piano_staff = abjad.StaffGroup(
-            [viola_1_staff, viola_2_staff],
-            lilypond_type="PianoStaff",
-            name="Piano_Staff",
+        quartet_staff = abjad.StaffGroup(
+            [flute_staff, bb_clarinet_staff, guitar_staff, viola_staff],
+            lilypond_type="Quartet_Staff",
+            name="Quartet_Staff",
             tag=tag,
         )
         music_context = abjad.Context(
-            [piano_staff],
+            [quartet_staff],
             lilypond_type="MusicContext",
             name="Music_Context",
             tag=tag,
@@ -160,17 +133,16 @@ class ScoreTemplate(abjad.ScoreTemplate):
     def voice_abbreviations(self):
         """
         Gets voice abbreviations.
-
         ..  container:: example
-
-            >>> score_template = ins_wasser.ScoreTemplate()
+            >>> score_template = rill.ScoreTemplate()
             >>> abjad.f(score_template.voice_abbreviations)
             abjad.OrderedDict(
                 [
-                    ('va1', 'Viola_I_Music_Voice'),
-                    ('va2', 'Viola_II_Music_Voice'),
+                    ('fl', 'Flute_Music_Voice'),
+                    ('bbcl', 'Bb_Clarinet_Music_Voice'),
+                    ('guit', 'Guitar_Music_Voice'),
+                    ('vla', 'Viola_Music_Voice'),
                     ]
                 )
-
         """
         return super(ScoreTemplate, self).voice_abbreviations
