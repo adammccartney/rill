b05bf968b7a8d56d720f0c8c06e127da93bbe94c
adammccartney <adam@mur.at>
2020-05-09 10:56:59 +0200

    created score template

--- rill/materials/score-segment-template.py
+++ rill/materials/score-segment-template.py
@@ -1,7 +1,7 @@
 import abjad
 
 """
-Defines scoretemplate, instance of Score class. Segments appended later. 
+Defines scoretemplate, subclass of abjad.ScoreTemplate. Segments appended later. 
 """
 
 
@@ -32,55 +32,89 @@ class ScoreTemplate(abjad.ScoreTemplate):
         """
         Calls score template.
         """
-        site = "ins_wasser.ScoreTemplate.__call__()"
+        site = "rill.ScoreTemplate.__call__()"
         tag = abjad.Tag(site)
 
         # GLOBAL CONTEXT
         global_context = self._make_global_context()
 
-        # VIOLA 1
-        markup_voice = abjad.Voice(name="Viola_I_Markup_Voice", tag=tag)
-        music_voice = abjad.Voice(name="Viola_I_Music_Voice", tag=tag)
-        dynamics_voice = abjad.Voice(name="Viola_I_Dynamics_Voice", tag=tag)
-        viola_1_staff = abjad.Staff(
+        # Flute
+        markup_voice = abjad.Voice(name="Flute_Markup_Voice", tag=tag)
+        music_voice = abjad.Voice(name="Flute_Music_Voice", tag=tag)
+        dynamics_voice = abjad.Voice(name="Flute_Dynamics_Voice", tag=tag)
+        flute_staff = abjad.Staff(
             [markup_voice, music_voice, dynamics_voice],
             simultaneous=True,
-            name="Viola_I",
+            name="Flute",
             tag=tag,
         )
         abjad.annotate(
-            viola_1_staff,
+            flute_staff,
             "default_instrument",
-            ins_wasser.instruments["ViolaI"],
+            rill.instruments["Flute"],
         )
-        abjad.annotate(viola_1_staff, "default_clef", abjad.Clef("alto"))
+        abjad.annotate(flute_staff, "default_clef", abjad.Clef("treble"))
 
-        # VIOLA 2
-        markup_voice = abjad.Voice(name="Viola_II_Markup_Voice", tag=tag)
-        music_voice = abjad.Voice(name="Viola_II_Music_Voice", tag=tag)
-        dynamics_voice = abjad.Voice(name="Viola_II_Dynamics_Voice", tag=tag)
-        viola_2_staff = abjad.Staff(
+        # Bb_Clarinet
+        markup_voice = abjad.Voice(name="Bb_Clarinet_Markup_Voice", tag=tag)
+        music_voice = abjad.Voice(name="Bb_Clarinet_Music_Voice", tag=tag)
+        dynamics_voice = abjad.Voice(name="Bb_Clarinet_Dynamics_Voice", tag=tag)
+        bb_clarinet_staff = abjad.Staff(
             [markup_voice, music_voice, dynamics_voice],
             simultaneous=True,
-            name="Viola_II",
+            name="Bb_Clarinet",
             tag=tag,
         )
         abjad.annotate(
-            viola_2_staff,
+            bb_clarinet_staff,
             "default_instrument",
-            ins_wasser.instruments["ViolaII"],
+            rill.instruments["Bb_Clarinet"],
+        )
+        abjad.annotate(bb_clarinet_staff, "default_clef", abjad.Clef("treble"))
+
+        # Guitar 
+        markup_voice = abjad.Voice(name="Guitar_Markup_Voice", tag=tag)
+        music_voice = abjad.Voice(name="Guitar_Music_Voice", tag=tag)
+        dynamics_voice = abjad.Voice(name="Guitar_Dynamics_Voice", tag=tag)
+        guitar_staff = abjad.Staff(
+            [markup_voice, music_voice, dynamics_voice],
+            simultaneous=True,
+            name="Guitar",
+            tag=tag,
+        )
+        abjad.annotate(
+            guitar_staff,
+            "default_instrument",
+            rill.instruments["Guitar"],
+        )
+        abjad.annotate(guitar_staff, "default_clef", abjad.Clef("treble"))
+
+        # VIOLA  
+        markup_voice = abjad.Voice(name="Viola_Markup_Voice", tag=tag)
+        music_voice = abjad.Voice(name="Viola_Music_Voice", tag=tag)
+        dynamics_voice = abjad.Voice(name="Viola_Dynamics_Voice", tag=tag)
+        viola_staff = abjad.Staff(
+            [markup_voice, music_voice, dynamics_voice],
+            simultaneous=True,
+            name="Viola",
+            tag=tag,
+        )
+        abjad.annotate(
+            viola_staff,
+            "default_instrument",
+            rill.instruments["Viola"],
         )
         abjad.annotate(viola_2_staff, "default_clef", abjad.Clef("alto"))
 
         # SCORE
-        piano_staff = abjad.StaffGroup(
-            [viola_1_staff, viola_2_staff],
-            lilypond_type="PianoStaff",
-            name="Piano_Staff",
+        quartet_staff = abjad.StaffGroup(
+            [flute_staff, bb_clarinet_staff, guitar_staff, viola_staff],
+            lilypond_type="Quartet_Staff",
+            name="Quartet_Staff",
             tag=tag,
         )
         music_context = abjad.Context(
-            [piano_staff],
+            [quartet_staff],
             lilypond_type="MusicContext",
             name="Music_Context",
             tag=tag,
@@ -100,12 +134,14 @@ class ScoreTemplate(abjad.ScoreTemplate):
         """
         Gets voice abbreviations.
         ..  container:: example
-            >>> score_template = ins_wasser.ScoreTemplate()
+            >>> score_template = rill.ScoreTemplate()
             >>> abjad.f(score_template.voice_abbreviations)
             abjad.OrderedDict(
                 [
-                    ('va1', 'Viola_I_Music_Voice'),
-                    ('va2', 'Viola_II_Music_Voice'),
+                    ('fl', 'Flute_Music_Voice'),
+                    ('bbcl', 'Bb_Clarinet_Music_Voice'),
+                    ('guit', 'Guitar_Music_Voice'),
+                    ('vla', 'Viola_Music_Voice'),
                     ]
                 )
         """
