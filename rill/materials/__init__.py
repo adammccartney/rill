ac6fd66c5deb7796b10bf1b7def2c03c867bbb04
adammccartney <adam@mur.at>
2020-10-17 15:38:41 +0200

    recursive change to init

--- rill/materials/__init__.py
+++ rill/materials/__init__.py
@@ -1,4 +1,5 @@
 from .instruments.definition import instruments
+from .instruments import markups
 from .margin_markups.definition import margin_markups
 from .metronome_marks.definition import metronome_marks
 from .pitch.definition import *
