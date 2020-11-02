41c08af99af37f5da1f9b88c3252c7643ee44e90
adammccartney <adam@mur.at>
2020-11-02 10:30:37 +0100

    Found working structure for initializing the materials on a per_instrument_and_segment basis

--- rill/materials/music_init_data/music_data_generator.py
+++ rill/materials/music_init_data/music_data_generator.py
@@ -1,25 +1,26 @@
 import abjad
 import rill
 
-from collections import namedtuple
 from dataclasses import dataclass
 from typing import List
 
-from rill.tools.AttachmentMaker import *
-from rill.materials.pitch.definition import *
-
+from rill.tools.AttachmentMaker import (AttachmentMaker,
+                                        AccentAttachmentMaker)
+from rill.materials.pitch.definition import chord_voice
 
 @dataclass
 class MusicInitData:
     pitches: abjad.CyclicTuple
     attachments: List[AttachmentMaker]
-    denonimator: int
-    time_signature_pairs: List[tuple[int, int]]
+    denominator: int
+    time_signature_pairs: List[tuple]
     talea: List[int]
 
 
 # Pitches
-v1_pitch_materials = rill.chord_voice["black"][5][0:3] + rill.chord_voice["red"][5][0:3]
+v1_pitch_materials = rill.chord_voice["black"][5][0:3]
+v1_pitch_materials += rill.chord_voice["red"][5][0:3]
+
 v1_pitches_segment_A = abjad.CyclicTuple(v1_pitch_materials)
 
 tenuto_attachment_maker = AccentAttachmentMaker(
@@ -37,20 +38,13 @@ v1_denominator_segment_A = 16
 v1_tsig_pairs_segment_A = [(4, 4), (3, 4), (3, 4), (4, 4), (3, 4), (3, 4)]
 v1_talea_segment_A = [1, 2, -3, 4]
 
-v1_args_segment_A = MusicInitData(
-                     v1_pitches_segment_A,
-                     v1_attachments_segment_A,
-                     v1_denominator_segment_A,
-                     v1_tsig_pairs_segment_A,
-                     v1_talea_segment_A)
-
-MuData = namedtuple('instrument', ['pitches',
-                                   'attachments',
-                                   'denominator',
-                                   'ts_pairs',
-                                   'talea'])
-
-#Flute1_segment_A = MuData('Flute1', [v1_args_segment_A])
+v1_init_data_segment_A = MusicInitData(
+    pitches=v1_pitches_segment_A,
+    attachments=v1_attachments_segment_A,
+    denominator=v1_denominator_segment_A,
+    time_signature_pairs=v1_tsig_pairs_segment_A,
+    talea=v1_talea_segment_A,
+)
 
 if __name__ == '__main__':
-    print(v1_args_segment_A)
+    print(v1_init_data_segment_A)
