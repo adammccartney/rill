5e5004e439d9bac60456a1ef641d7464b99bc6be
adammccartney <adam@mur.at>
2020-11-04 20:23:58 +0100

    going to test in real segment scope

--- rill/segments/genMusicData.py
+++ rill/segments/genMusicData.py
@@ -1,7 +1,29 @@
+import sys
+
+var = sys.argv[1]
+segment_name = f"segment_{var}"  # used to recall music data from db
+
+
+pitch_data_key = f"{segment_name}_pitches"
+talea_data_key = f"{segment_name}_talea"
+
 music_data = f"""
+import shelve
+import abjad
+
+from pathlib import Path
+
 from rill.materials.music_init_data.definition import InstrumentMusicData
 from rill.materials.music_init_data.definition import SegmentMusicData
 
+
+up_two_dirs = Path.cwd().parents[1] # ../segments/
+db_path = up_two_dirs / 'materials' / 'music_data' / 'music_data_shelve'
+
+db = shelve.open(str(db_path))
+
+{segment_name}_pitch_data = db['{pitch_data_key}']
+{segment_name}_talea_data = db['{talea_data_key}']
 
 Flute1_music_data = InstrumentMusicData()
 Flute2_music_data = InstrumentMusicData()
