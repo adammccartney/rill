dd0f09130d82eec4a858353359220d1b6f247587
adammccartney <adam@mur.at>
2020-11-04 11:49:33 +0100

    Defined getters and setters that return pitch segment as a string that can be used for initialization in the local scope of each segment

--- rill/materials/music_init_data/definition.py
+++ rill/materials/music_init_data/definition.py
@@ -1,3 +1,4 @@
+import re
 import abjad
 
 from dataclasses import dataclass, field
@@ -89,6 +90,90 @@ class SegmentMusicData:
         return getattr(self, key)
 
 
+def make_empty_string():
+    return ""
+
+
+@dataclass
+class SegmentPitchData:
+    "Stores pitch data per segment as string"
+    _chord_voice1: str = field(default_factory=make_empty_string)
+    _chord_voice2: str = field(default_factory=make_empty_string)
+    _chord_voice3: str = field(default_factory=make_empty_string)
+    _chord_voice4: str = field(default_factory=make_empty_string)
+    _melody_voice: str = field(default_factory=make_empty_string)
+    _tremolo_voice1: str = field(default_factory=make_empty_string)
+    _tremolo_voice2: str = field(default_factory=make_empty_string)
+
+    def _replace_brackets(self, pitch_segment_as_string):
+        pitch_segment = re.sub(r'<|>', r'"', pitch_segment_as_string)
+        return pitch_segment
+
+    @property
+    def chord_voice1(self) -> str:
+        pitch_segment = self._chord_voice1
+        pitch_segment_as_string = self._replace_brackets(pitch_segment)
+        return pitch_segment_as_string
+
+    @chord_voice1.setter
+    def chord_voice1(self, v: str) -> None:
+        self._chord_voice1 = v
+
+    @property
+    def chord_voice2(self) -> str:
+        pitch_segment_as_string = self._replace_brackets(self._chord_voice2)
+        return pitch_segment_as_string
+
+    @chord_voice2.setter
+    def chord_voice2(self, v: str) -> None:
+        self._chord_voice2 = v
+
+    @property
+    def chord_voice3(self) -> str:
+        pitch_segment_as_string = self._replace_brackets(self._chord_voice3)
+        return pitch_segment_as_string
+
+    @chord_voice3.setter
+    def chord_voice3(self, v: str) -> None:
+        self._chord_voice3 = v
+
+    @property
+    def chord_voice4(self) -> str:
+        pitch_segment_as_string = self._replace_brackets(self._chord_voice4)
+        return pitch_segment_as_string
+
+    @chord_voice4.setter
+    def chord_voice4(self, v: str) -> None:
+        self._chord_voice4 = v
+
+    @property
+    def melody_voice(self) -> str:
+        pitch_segment_as_string = self._replace_brackets(self._melody_voice)
+        return pitch_segment_as_string
+
+    @melody_voice.setter
+    def melody_voice(self, v: str) -> None:
+        self._melody_voice = v
+
+    @property
+    def tremolo_voice1(self) -> str:
+        pitch_segment_as_string = self._replace_brackets(self._tremolo_voice1)
+        return pitch_segment_as_string
+
+    @tremolo_voice1.setter
+    def tremolo_voice1(self, v: str) -> None:
+        self._tremolo_voice1 = v
+
+    @property
+    def tremolo_voice2(self) -> str:
+        pitch_segment_as_string = self._replace_brackets(self._tremolo_voice2)
+        return pitch_segment_as_string
+
+    @tremolo_voice2.setter
+    def tremolo_voice2(self, v: str) -> None:
+        self._tremolo_voice1 = v
+
+
 if __name__ == '__main__':
    # default_init_data = InstrumentMusicData()
    # print(default_init_data)
@@ -101,7 +186,15 @@ if __name__ == '__main__':
     #segment_defaults = make_segment_music_default()
     #for instrument_music_data in segment_defaults:
     #    print(instrument_music_data)
-    segment_test_music_data = SegmentMusicData()
-    segment_dict = segment_test_music_data.instrument_music_data
-    for instrument in instruments:
-        print(instrument, ": ", segment_dict[instrument], '\n')
+    #segment_test_music_data = SegmentMusicData()
+    #segment_dict = segment_test_music_data.instrument_music_data
+    #for instrument in instruments:
+    #    print(instrument, ": ", segment_dict[instrument], '\n')
+
+    default_pitch_materials = chord_voice["black"][5][0:3]
+    default_pitch_materials += chord_voice["red"][5][0:3]
+    default_pitch_data = str(default_pitch_materials)
+    test_segment_pitch_data = SegmentPitchData(_chord_voice3=default_pitch_data)
+    test_segment_cv3_pitch_segment = test_segment_pitch_data.chord_voice3
+    print(test_segment_pitch_data)
+    print(test_segment_cv3_pitch_segment)
