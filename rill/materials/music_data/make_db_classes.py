7c06529499cf6bb713333d6723940a6713af1809
adammccartney <adam@mur.at>
2020-11-04 20:13:51 +0100

    Working defaults for pitch and talea, tested with local segment variable

--- rill/materials/music_data/make_db_classes.py
+++ rill/materials/music_data/make_db_classes.py
@@ -2,20 +2,22 @@ import shelve
 
 from rill.materials.music_init_data.definition import (
     SegmentPitchData,
+    SegmentTaleaData,
 )
 
 from rill.materials.pitch.definition import chord_voice
 
 db = shelve.open('music_data_shelve')
 
-segmentA_cv3_pitch_materials = chord_voice["black"][5][0:3]
-segmentA_cv3_pitch_materials += chord_voice["red"][5][0:3]
-segmentA_cv3_pitches = str(segmentA_cv3_pitch_materials)
+segment_TEST_cv3_pitch_materials = chord_voice["black"][5][0:3]
+segment_TEST_cv3_pitch_materials += chord_voice["red"][5][0:3]
+segment_TEST_cv3_pitches = str(segment_TEST_cv3_pitch_materials)
 
-segmentA_pitch_data = SegmentPitchData(_chord_voice3=segmentA_cv3_pitches)
+segment_TEST_pitch_data = SegmentPitchData(_chord_voice3=segment_TEST_cv3_pitches)
+segment_TEST_talea_data = SegmentTaleaData()
 
-
-db['segment_A_pitches'] = segmentA_pitch_data
+db['segment_TEST_pitches'] = segment_TEST_pitch_data
+db['segment_TEST_talea'] = segment_TEST_talea_data
 
 db.close()
 
@@ -23,6 +25,8 @@ if __name__ == '__main__':
     db = shelve.open('music_data_shelve')
     for key in db:
         print(key)
-    segment_A_pitch_data = db['segment_A_pitches']
-    print(segment_A_pitch_data.chord_voice3)
+    segment_TEST_pitch_data = db['segment_TEST_pitches']
+    segment_TEST_talea_data = db['segment_TEST_talea']
+    print(segment_TEST_pitch_data.chord_voice3)
+    print(segment_TEST_talea_data)
     db.close()
