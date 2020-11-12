e9313863f198b7acb795a0efd920625a8be2dbe3
adammccartney <adam@mur.at>
2020-11-12 15:58:23 +0100

    pitch overrides on clarinet, vibes and viola

--- rill/segments/segment_B/music_data.py
+++ rill/segments/segment_B/music_data.py
@@ -30,19 +30,23 @@ segment_B_pulse_counts = segment_B_talea_data.pulse_counts
 segment_B_pedal_counts = segment_B_talea_data.pedal_tone_counts
 
 segment_B_Flute1_instr_pdref = segment_B_instr_pdref.Flute1
-segment_B_Flute1_pd = getattr(segment_B_pitch_data, segment_B_Flute1_instr_pdref)
+segment_B_Flute1_pd = getattr(segment_B_pitch_data,
+                              segment_B_Flute1_instr_pdref)
 segment_B_Flute1_pitch_segment = abjad.PitchSegment(segment_B_Flute1_pd)
 Flute1_music_data = InstrumentMusicData()
 Flute1_music_data.pitches = segment_B_Flute1_pitch_segment
 Flute1_music_data.talea = segment_B_choral1_counts
+Flute1_music_data.denominator = 8
 segment_music_data.Flute1 = Flute1_music_data
 
 segment_B_Flute2_instr_pdref = segment_B_instr_pdref.Flute2
-segment_B_Flute2_pd = getattr(segment_B_pitch_data, segment_B_Flute2_instr_pdref)
+segment_B_Flute2_pd = getattr(segment_B_pitch_data,
+                              segment_B_Flute2_instr_pdref)
 segment_B_Flute2_pitch_segment = abjad.PitchSegment(segment_B_Flute2_pd)
 Flute2_music_data = InstrumentMusicData()
 Flute2_music_data.pitches = segment_B_Flute2_pitch_segment
 Flute2_music_data.talea = segment_B_choral1_counts
+Flute2_music_data.denominator = 8
 segment_music_data.Flute2 = Flute2_music_data
 
 segment_B_Flute3_instr_pdref = segment_B_instr_pdref.Flute3
@@ -51,6 +55,7 @@ segment_B_Flute3_pitch_segment = abjad.PitchSegment(segment_B_Flute3_pd)
 Flute3_music_data = InstrumentMusicData()
 Flute3_music_data.pitches = segment_B_Flute3_pitch_segment
 Flute3_music_data.talea = segment_B_choral2_counts
+Flute3_music_data.denominator = 8
 segment_music_data.Flute3 = Flute3_music_data
 
 segment_B_Flute4_instr_pdref = segment_B_instr_pdref.Flute4
@@ -59,6 +64,7 @@ segment_B_Flute4_pitch_segment = abjad.PitchSegment(segment_B_Flute4_pd)
 Flute4_music_data = InstrumentMusicData()
 Flute4_music_data.pitches = segment_B_Flute4_pitch_segment
 Flute4_music_data.talea = segment_B_choral2_counts
+Flute4_music_data.denominator = 8
 segment_music_data.Flute4 = Flute4_music_data
 
 segment_B_Bbclarinet1_instr_pdref = segment_B_instr_pdref.Bbclarinet1
@@ -66,23 +72,25 @@ segment_B_Bbclarinet1_pd = getattr(segment_B_pitch_data, segment_B_Bbclarinet1_i
 segment_B_Bbclarinet1_pitch_segment = abjad.PitchSegment(
 segment_B_Bbclarinet1_pd)
 Bbclarinet1_music_data = InstrumentMusicData()
-Bbclarinet1_music_data.pitches = segment_B_Bbclarinet1_pitch_segment
+Bbclarinet1_music_data.pitches = segment_B_Bbclarinet1_pitch_segment.transpose(-24)
 Bbclarinet1_music_data.denominator = 4
 Bbclarinet1_music_data.talea = segment_B_pulse_counts
 segment_music_data.Bbclarinet1 = Bbclarinet1_music_data
 
 segment_B_Vibraphone_instr_pdref = segment_B_instr_pdref.Vibraphone
-segment_B_Vibraphone_pd = getattr(segment_B_pitch_data, segment_B_Vibraphone_instr_pdref)
+segment_B_Vibraphone_pd = getattr(segment_B_pitch_data,
+                                  segment_B_Vibraphone_instr_pdref)
 segment_B_Vibraphone_pitch_segment = abjad.PitchSegment(
-segment_B_Vibraphone_pd)
+    segment_B_Vibraphone_pd)
 Vibraphone_music_data = InstrumentMusicData()
-Vibraphone_music_data.pitches = segment_B_Vibraphone_pitch_segment
+Vibraphone_music_data.pitches = segment_B_Vibraphone_pitch_segment.transpose(-24)
 Vibraphone_music_data.denominator = 16
 Vibraphone_music_data.talea = segment_B_pulse_counts
 segment_music_data.Vibraphone = Vibraphone_music_data
 
 segment_B_Violin1_instr_pdref = segment_B_instr_pdref.Violin1
-segment_B_Violin1_pd = getattr(segment_B_pitch_data, segment_B_Violin1_instr_pdref)
+segment_B_Violin1_pd = getattr(segment_B_pitch_data,
+                               segment_B_Violin1_instr_pdref)
 segment_B_Violin1_pitch_segment = abjad.PitchSegment(segment_B_Violin1_pd)
 Violin1_music_data = InstrumentMusicData()
 Violin1_music_data.pitches = segment_B_Violin1_pitch_segment
@@ -92,7 +100,8 @@ Violin1_music_data.denominator = 8
 segment_music_data.Violin1 = Violin1_music_data
 
 segment_B_Violin2_instr_pdref = segment_B_instr_pdref.Violin2
-segment_B_Violin2_pd = getattr(segment_B_pitch_data, segment_B_Violin2_instr_pdref)
+segment_B_Violin2_pd = getattr(segment_B_pitch_data,
+                               segment_B_Violin2_instr_pdref)
 segment_B_Violin2_pitch_segment = abjad.PitchSegment(segment_B_Violin2_pd)
 Violin2_music_data = InstrumentMusicData()
 Violin2_music_data.pitches = segment_B_Violin2_pitch_segment
@@ -101,7 +110,8 @@ Violin2_music_data.denominator = 8
 segment_music_data.Violin2 = Violin2_music_data
 
 segment_B_Violin3_instr_pdref = segment_B_instr_pdref.Violin3
-segment_B_Violin3_pd = getattr(segment_B_pitch_data, segment_B_Violin3_instr_pdref)
+segment_B_Violin3_pd = getattr(segment_B_pitch_data,
+                               segment_B_Violin3_instr_pdref)
 segment_B_Violin3_pitch_segment = abjad.PitchSegment(segment_B_Violin3_pd)
 Violin3_music_data = InstrumentMusicData()
 Violin3_music_data.pitches = segment_B_Violin3_pitch_segment
@@ -154,7 +164,7 @@ segment_B_Viola_instr_pdref = segment_B_instr_pdref.Viola
 segment_B_Viola_pd = getattr(segment_B_pitch_data, segment_B_Viola_instr_pdref)
 segment_B_Viola_pitch_segment = abjad.PitchSegment(segment_B_Viola_pd)
 Viola_music_data = InstrumentMusicData()
-Viola_music_data.pitches = segment_B_Viola_pitch_segment
+Viola_music_data.pitches = segment_B_Viola_pitch_segment.transpose(-12)
 Viola_music_data.talea = segment_B_syncopated_counts
 segment_music_data.Viola = Viola_music_data
 
