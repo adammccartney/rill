570f19a6cb2f00ed57f9ef9ede6897e0bf185d95
adammccartney <adam@mur.at>
2020-11-16 12:20:20 +0100

    Defined wrapper classes to handle the various attachment definitions

--- rill/materials/attachments/definition.py
+++ rill/materials/attachments/definition.py
@@ -1,40 +1,96 @@
 import abjad
+import rill
+
+from dataclasses import dataclass, field
 
 from rill.tools.AttachmentMaker import (AttachmentMaker,
                                         AccentAttachmentMaker,
                                         MarkupFirstAttachmentMaker)
 from rill.tools.MarkupLibrary import MarkupLibrary as markup
 
-tenuto_attachment_maker = AccentAttachmentMaker(
+
+"""
+Definitions and wrapper (data)classes for attachments
+
+"""
+
+def make_tenuto_attachment_maker():
+    tenuto_attachment_maker = AccentAttachmentMaker(
         selector=abjad.select().logical_ties(),
         attachment=abjad.Articulation("tenuto")
     )
-staccato_attachment_maker = AccentAttachmentMaker(
-    selector=abjad.select().logical_ties(),
-    attachment=abjad.Staccato()
-    )
+    return tenuto_attachment_maker
 
-ord_attachment_maker = MarkupFirstAttachmentMaker(
+
+def make_staccato_attachment_maker():
+    staccato_attachment_maker = AccentAttachmentMaker(
+        selector=abjad.select().logical_ties(),
+        attachment=abjad.Staccato()
+    )
+    return staccato_attachment_maker
+
+@dataclass
+class AccentAttachmentMakerData:
+    tenuto: AccentAttachmentMaker = field(
+        default_factory=make_tenuto_attachment_maker)
+    staccato: AccentAttachmentMaker = field(
+        default_factory=make_staccato_attachment_maker)
+
+
+def make_ord_attachment_maker():
+    ord_attachment_maker = MarkupFirstAttachmentMaker(
         selector=abjad.select(),
-        attachment=markup.ord()
+        attachment=markup.ordinario()
     )
+    return ord_attachment_maker
 
-tasto_attachment_maker = MarkupFirstAttachmentMaker(
+
+def make_tasto_attachment_maker():
+    tasto_attachment_maker = MarkupFirstAttachmentMaker(
         selector=abjad.select(),
         attachment=markup.tasto()
     )
+    return tasto_attachment_maker
 
-flaut_attachment_maker = MarkupFirstAttachmentMaker(
+
+def make_flaut_attachment_maker():
+    flaut_attachment_maker = MarkupFirstAttachmentMaker(
         selector=abjad.select(),
         attachment=markup.flaut()
     )
+    return flaut_attachment_maker
 
-flaut_pont_attachment_maker = MarkupFirstAttachmentMaker(
+
+def make_flaut_pont_attachment_maker():
+    flaut_pont_attachment_maker = MarkupFirstAttachmentMaker(
         selector=abjad.select(),
         attachment=markup.flaut_pont()
     )
+    return flaut_pont_attachment_maker
 
-pont_attachment_maker = MarkupFirstAttachmentMaker(
+
+def make_pont_attachment_maker():
+    pont_attachment_maker = MarkupFirstAttachmentMaker(
         selector=abjad.select(),
         attachment=markup.pont()
+    )
+    return pont_attachment_maker
+
+
+@dataclass
+class BowPositionData(object):
+    ordinario: MarkupFirstAttachmentMaker = field(
+        default_factory=make_ord_attachment_maker
+    )
+    tasto: MarkupFirstAttachmentMaker = field(
+        default_factory=make_tasto_attachment_maker
+    )
+    flaut: MarkupFirstAttachmentMaker = field(
+        default_factory=make_flaut_attachment_maker
+    )
+    pont: MarkupFirstAttachmentMaker = field(
+        default_factory=make_pont_attachment_maker
+    )
+    flaut_pont: MarkupFirstAttachmentMaker = field(
+        default_factory=make_flaut_pont_attachment_maker
     )
