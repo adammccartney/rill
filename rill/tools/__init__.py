bb3a9fd49c8ef294781175b481a778d3b9915a3c
adammccartney <adam@mur.at>
2020-11-22 10:35:04 +0100

    removed redundant includes

--- rill/tools/__init__.py
+++ rill/tools/__init__.py
@@ -1,26 +1,5 @@
-from .accents import *
-from .barlines import barline, repeat
-from .clef import clef
-from .FuzzyHarmony import Diad as Diad
-from .FuzzyHarmony import FuzzyHarmony as FuzzyHarmony
-from .grace import *
-from .line_break import line_break
-from .material_methods import (
-    get_pitch_classes,
-    make_decimo_diad,
-    make_diads,
-    make_augmented_stream,
-    transpose,
-    transpose_segment,
-)
-
 from .MarkupLibrary import MarkupLibrary
 from .MusicMaker import *
-from .notehead_overrides import *
-from .ottava import ottava
 from .ScoreTemplate import ScoreTemplate
 from .SegmentMaker import SegmentMaker
 from .set_build_path import build_path
-from .tie import tie
-from .time_signature import time_signature
-from .tremolo import tremolo
