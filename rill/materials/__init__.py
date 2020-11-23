cec8eee1f11c639c040ed6a1e902a1ac04061f87
adammccartney <adam@mur.at>
2020-11-23 09:32:09 +0100

    trimmed redundant includes from materials & subdirectories

--- rill/materials/__init__.py
+++ rill/materials/__init__.py
@@ -1,4 +1,3 @@
 from .instruments.definition import instruments
-from .margin_markups.definition import margin_markups
 from .metronome_marks.definition import metronome_marks
 from .pitch.definition import *
