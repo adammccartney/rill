546f7fdfcdcd468cd7bf3d39fa90824f82dafafc
Thomas Grill <gr@grrrr.org>
2020-11-30 23:32:24 +0100

    Fixing requirements

--- setup.py
+++ setup.py
@@ -6,7 +6,11 @@ if __name__ == "__main__":
     setuptools.setup(
         author="Adam McCartney",
         author_email="adam@mur.at",
-        install_requires=("abjad",),
+        install_requires=(
+            "abjad>=3.1", 
+            "abjad-ext-tonality", 
+            "abjad-ext-rmakers"
+        ),
         name="rill",
         packages=("rill",),
         url="https://github.com/adammccartney/rill",
