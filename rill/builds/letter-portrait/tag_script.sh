c1405a2f36538f1974e72829bb27a8fd46b0ae55
Thomas Grill <gr@grrrr.org>
2020-12-01 19:18:23 +0100

    Made tagging more explicit and Make-conform

--- rill/builds/letter-portrait/tag_script.sh
+++ rill/builds/letter-portrait/tag_script.sh
@@ -3,9 +3,6 @@
 # tag_script.sh
 # Simple shell script to generate a version tag for the score front page
 
-touch ./tag_script_output.tex
 branch=$(git branch --show-current)
 SHA=$(git rev-parse --short HEAD)
-echo $branch $SHA > ./tag_script_output.tex
-
-exit 0
+echo $branch $SHA
