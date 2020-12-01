f1a6c8e263d22dce6fee37b8d1a1690d1f1d8f2d
Thomas Grill <thomas@rottingsounds.org>
2020-12-01 19:23:50 +0100

    Make branch info compatible with all gits

--- rill/builds/letter-portrait/tag_script.sh
+++ rill/builds/letter-portrait/tag_script.sh
@@ -3,6 +3,6 @@
 # tag_script.sh
 # Simple shell script to generate a version tag for the score front page
 
-branch=$(git branch --show-current)
+branch=$(git status -b --porcelain | sed  's/^## \([a-zA-Z_0-9]*\)\.\.\.\(.*\)$/\1/' | head -n 1)
 SHA=$(git rev-parse --short HEAD)
 echo $branch $SHA
