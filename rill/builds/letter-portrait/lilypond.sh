1296b26879dc75df2d6b66510256423c082bbe5a
Thomas Grill <thomas@rottingsounds.org>
2020-12-08 17:53:45 +0100

    leave messages to stdout

--- rill/builds/letter-portrait/lilypond.sh
+++ rill/builds/letter-portrait/lilypond.sh
@@ -3,9 +3,8 @@
 trap 'rm -f "$TMPFILE"' EXIT
 
 TMPFILE=$(mktemp) || exit 1
-echo $TMPFILE
 
-LANG=C lilypond $1 >/dev/null 2>$TMPFILE
+LANG=C lilypond $1 2>$TMPFILE
 
 res=`grep -E "warning: (barcheck failed|strange time signature found)" $TMPFILE`
 
