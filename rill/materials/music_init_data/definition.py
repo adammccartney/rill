11cf1dde81514e58857a4bea5fe10dfe74100aa2
Thomas Grill <thomas@rottingsounds.org>
2021-01-25 17:56:34 +0100

    Erosion

--- rill/materials/music_init_data/definition.py
+++ rill/materials/music_init_data/definition.py
@@ -24,7 +24,7 @@ def make_default_attachments():
     #    attachment=abjad.Articulation("tenuto")
     #)
 
-    #staccato_attachment_maker = AccentAttachmentMaker(
+    #staccato_cttachment_maker = AccentAttachmentMaker(
     #    selector=abjad.select().logical_ties(),
     #    attachment=abjad.Staccato()
     #)
