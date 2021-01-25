54b31cc70aee9e2b2a3db05947d3864e8aaeac74
Thomas Grill <thomas@rottingsounds.org>
2021-01-25 18:14:20 +0100

    Erosion

--- rill/materials/music_init_data/definition.py
+++ rill/materials/music_init_data/definition.py
@@ -413,7 +413,7 @@ class InstrumentPitchData:
            (ref == 'tremolo_voice2'):
             self._Flute1 = ref
         else:
-            ValueError(ref, "is not a valid reference for pdata")
+            ValueError(ref, "is not a valid reference for pdada")
 
     @property
     def Flute2(self) -> str:
