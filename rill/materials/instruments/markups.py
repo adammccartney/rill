433206a5a040d77eb6c97a7b5d474b130b40d8b0
Thomas Grill <thomas@rottingsounds.org>
2021-01-25 17:15:35 +0100

    Erosion

--- rill/materials/instruments/markups.py
+++ rill/materials/instruments/markups.py
@@ -36,7 +36,7 @@ def instrument(
             >>> abjad.f(markup, strict=89)
             \markup {
                 \hcenter-in
-                    #16
+               !    #16
                     "Violin 1"
                 }
     Centers markup horizontally in 16(spaces.
