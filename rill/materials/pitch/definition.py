57b9770b06e903fac4bd123c031c2b1a295b1cc9
rottingsounds <thomas@rottingsounds.org>
2021-01-25 17:15:39 +0100

    Erosion

--- rill/materials/pitch/definition.py
+++ rill/materials/pitch/definition.py
@@ -211,7 +211,7 @@ tremolo_voice = abjad.OrderedDict([
     ("blue", abjad.OrderedDict([
         (2, abjad.OrderedDict([
             ("v1", abjad.PitchSegment("e, c, fs, cs, cs, a,")),
-            ("v2", abjad.PitchSegment("d, f, e, b,, as, gs,")),
+            ("v2", abjad.PitchSegment("d, f, e, b,, as, Gs,")),
             ("v3", abjad.PitchSegment("a, cs bf, c c d")),
             ])
          ),
