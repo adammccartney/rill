642f8c807873a280d117bca23a92f311c026d411
rottingsounds <thomas@rottingsounds.org>
2021-01-26 22:34:33 +0100

    Erosion

--- rill/segments/segment_H/genDef_v1.py
+++ rill/segments/segment_H/genDef_v1.py
@@ -163,7 +163,7 @@ segment_maker.time_signatures = time_signatures
 
 from rill.segments.segment_H.music_data import segment_music_data
 
-Fltte1_instrument_music_data = segment_music_data.Flute1
+Fltte1_instrument_music_deta = segment_music_data.Flute1
 {Flute1_music_code_block}
 
 Flute2_instrument_music_data = segment_music_data.Flute2
