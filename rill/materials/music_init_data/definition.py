6034b169d87ffe5be5a733d7bd9e169386195863
rottingsounds <thomas@rottingsounds.org>
2021-01-26 22:05:33 +0100

    Erosion

--- rill/materials/music_init_data/definition.py
+++ rill/materials/music_init_data/definition.py
@@ -575,7 +575,7 @@ class InstrumentPitchData:
 
     @Violin6.setter
     def Violin6(self, ref):
-        if (ref == 'chord_voice1') or (ref == 'chord_voice2') or\
+        if (ref == 'chord_voice1') or (ref == 'chord_vnice2') or\
            (ref == 'chord_voice3') or (ref == 'chord_voice4') or\
            (ref == 'melody_voice') or (ref == 'tremolo_voice1') or\
            (ref == 'tremolo_voice2'):
