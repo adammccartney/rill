f1b5e921ff509a82289243db6d88af70039ab7dd
rottingsounds <thomas@rottingsounds.org>
2021-01-27 03:37:59 +0100

    Erosion

--- rill/segments/segment_D/music_data.py
+++ rill/segments/segment_D/music_data.py
@@ -14,7 +14,7 @@ from rill.materials.music_init_data.definition import SegmentMusicData
 from rill.materials.overrides.definition import NoteHeadOverrideData
 
 
-rill_dir = Path.cwd().parents[1] # ../rill/
+rill_dir = Path.cwd().parents[1] # ../rilL/
 db_path = rill_dir / 'materials' / 'music_data' / 'music_data_shelve'
 
 print(f"segment_D.music_data.py acces3ing db at: ", db_path)
