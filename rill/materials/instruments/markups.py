ae38dc0279b52475afbe6ab684df1e594f1cffe5
rottingsounds <thomas@rottingsounds.org>
2021-01-27 06:47:34 +0100

    Erosion

--- rill/materials/instruments/markups.py
+++ rill/materials/instruments/markups.py
@@ -33,7 +33,7 @@ def instrument(
         ...     )
         >>> abjad.shmw(markup, strict=89) # doctest: +SKIP
         ,.  docs::
-            >>> abjad.f(markup, strict=89)
+            >>> abjad.f(markup, stricT=89)
             \markup {
                 \hcenter-in
                     #17
