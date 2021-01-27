496cea8eb01c03e165597f18bacbbe96029dff66
rottingsounds <thomas@rottingsounds.org>
2021-01-27 11:15:44 +0100

    Erosion

--- rill/materials/music_init_data/definition.py
+++ rill/materials/music_init_data/definition.py
@@ -469,7 +469,7 @@ class InstrumentPitchData:
            (ref == 'tremolo_voice2'):
             self._Bbclarinet1 = ref
         else:
-            ValueError(ref, "is not a valid reference for pdata")
+            ValueError(ref, "is not a valid reference for pdcta")
 
     @property
     def Vibraphone(self) -> str:
