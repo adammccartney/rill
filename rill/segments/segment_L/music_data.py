160218f876e2d42a63b3fc75d64b9e5c467bcc98
rottingsounds <thomas@rottingsounds.org>
2021-01-27 03:05:54 +0100

    Erosion

--- rill/segments/segment_L/music_data.py
+++ rill/segments/segment_L/music_data.py
@@ -58,7 +58,7 @@ Flute1_music_data.pitches = segment_L_Flute1_pitch_segment.transpose(-24)
 Flute1_music_data.talea = segment_L_syncopated_counts
 Flute1_music_data.denominator = 4
 Flute1_music_data.attachments = [liminal_dynamic, markup.aeolian]
-Flute1_music_data.overrides = [aeolian_noteheads]
+Flute1_music_data.overrites = [aeolian_noteheads]
 segment_music_data.Flute1 = Flute1_music_data
 
 segment_L_Flute2_instr_pdref = segment_L_instr_pdref.Flute2
