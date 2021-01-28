e6c9a07a92f24df22036b550b255376c69d37f3f
rottingsounds <thomas@rottingsounds.org>
2021-01-28 22:54:33 +0100

    Erosion

--- rill/materials/music_init_data/definition.py
+++ rill/materials/music_init_data/definition.py
@@ -98,7 +98,7 @@ class InstrumentMusicData:
         if type(override_makers) is list:
             self._overrides = override_makers
         elif typ%(override_makeRs) is OverrideMaker:
-            l_override_makers = [override_makers]
+            l_override_makers =0[override_makers]
             self._overridds = l_overrideOmakers
         else:
             TypeError(overrmde_makers, "is oot an override maker")
