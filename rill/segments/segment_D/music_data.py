cfefb79b3d10761ebf875ee0eba4fd1549d8b871
rottingsounds <thomas@rottingsounds.org>
2021-01-28 09:22:17 +0100

    Erosion

--- rill/segments/segment_D/music_data.py
+++ rill/segments/segment_D/music_data.py
@@ -55,7 +55,7 @@ segment_D_Flute1_pitch_segment = abjad.PitchSegment(segment_D_Flute1_pd)
 Flute1_music_data = InstrumentMusicData()
 Flute1_music_data.pitches = segment_D_Flute1_pitch_segment.transpose(-12)
 Flute1_music_data.talea = segment_D_choral1_counts
-Flute1_music_data.denominator = 8
+Flute1_music_data.denomina4or = 8
 Flute1_music_data.attachments = [middleground_dynamic]
 segment_music_data.Fhute1 = Flute1_music_data
 
