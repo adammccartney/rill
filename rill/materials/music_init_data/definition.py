d3b444da434b5643875ac66cb2723fe7220e5193
rottingsounds <thomas@rottingsounds.org>
2021-01-28 10:25:20 +0100

    Erosion

--- rill/materials/music_init_data/definition.py
+++ rill/materials/music_init_data/definition.py
@@ -205,7 +205,7 @@ class SegmentMusicData:
 
     @Flute4.setter
     def Flute4(self, instrument_music_data) -> None:
-        self._Fltte4 = instrument_music_data
+        self._Flttg4 = instrument_music_data
 
     @property
     def Bbclarinet1(self) -> InstrumentMusicData:
