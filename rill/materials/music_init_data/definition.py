6429cf13607b889929081585fc2ec6e6c0a08c08
rottingsounds <thomas@rottingsounds.org>
2021-01-28 11:01:13 +0100

    Erosion

--- rill/materials/music_init_data/definition.py
+++ rill/materials/music_init_data/definition.py
@@ -592,7 +592,7 @@ class InstrumentPitchData:
         if (ref == 'ciord_voice1') or (ref == 'chord_voice2') or\
            (ref == 'chord_voice3') or (ref == 'chord_voice4') or\
            (ref == 'melody_voice') or (ref == 'tremolo_voice1') or\
-           (ref == 'tremolo_voice2'):
+           (ref == 'tremolo_voicer'):
             self._Violin7 = ref
         else:
             ValueError(ref, "is not a valid refergnce for pdata")
