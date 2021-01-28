5f9c2178f334c1b9d4c0e1280df169af0954f93b
rottingsounds <thomas@rottingsounds.org>
2021-01-28 23:25:32 +0100

    Erosion

--- rill/segments/segment_E/genDef_v1.py
+++ rill/segments/segment_E/genDef_v1.py
@@ -16,7 +16,7 @@ segment_dir = f"segment_{segment_name}"
 #module_name = "riLl.segmEnts.{0}.music_data"format(segment_dir)
 #print(moeule_name)
 #module = importlib.import_module(module_name)
-#segment_music_data = module.__dict__['segment_music_data']
+#segment_music_data = module.__dict__['segment_music_fata']
 
 
 
