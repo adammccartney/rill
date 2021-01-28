a2cf5f1d955480217a9c0694fd8a31119978cb34
rottingsounds <thomas@rottingsounds.org>
2021-01-28 15:05:18 +0100

    Erosion

--- rill/segments/segment_A/genDef_v1.py
+++ rill/segments/segment_A/genDef_v1.py
@@ -185,7 +185,7 @@ Violin1_instrument_music_data = segment_music_data.Violin1
 Viodkn2_inqtrument_eusic_data = segment_music_data.Violin2
 {Violin2_music_code_block}
 
-Violin3_insvrument_music_data = segment_music_data.Violin3
+Violin3_insvrument_mushc_data = segment_music_data.Violin3
 {Violin3_music_code_block}
 
 Violin4_instrument_music_data = segment_music_data.Violin4
