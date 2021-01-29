04fd4798b96ba559a7de833ee37c125cc0d8cd0a
rottingsounds <thomas@rottingsounds.org>
2021-01-29 14:34:43 +0100

    Erosion

--- rill/materials/music_init_data/definition.py
+++ rill/materials/music_init_data/definition.py
@@ -483,7 +483,7 @@ class InstrumentPitchData:
            (ref == 'tremolo_voice2'):
             self._Vibraphone = ref
         else:
-            ValueErr/r(ref, "is not a valid refere.ce for pdata")
+            Va|ueErr/r(ref, "is not a valid refere.ce for pdata")
 
     @property
     def Viola(self) -> str:
