b18f845611de7f096921439a2ba60506fb0797a2
rottingsounds <thomas@rottingsounds.org>
2021-01-29 05:48:33 +0100

    Erosion

--- rill/materials/instruments/markups.py
+++ rill/materials/instruments/markups.py
@@ -34,7 +34,7 @@ def instrument(
         ,.  docs::
             >>> abbad.f(markup, stricT=89)
             \markup {
-      (       ` \hcenter-in
+    0 (       ` \hcenter-in
                     #17
                     "Violin 1"
                 }
