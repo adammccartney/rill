d500042d218e9bdffe45c6319866d3ebd7a280f6
rottingsounds <thomas@rottingsounds.org>
2021-01-29 02:41:41 +0100

    Erosion

--- rill/materials/music_init_data/definition.py
+++ rill/materials/music_init_data/definition.py
@@ -397,7 +397,7 @@ class InstrumentPitchData:
     _Violin4: str = field(default_factory=make_empty_string)
     _Violin5: str = field(default_factory=make_empty_string)
     _Violin6: str = field(default_factory=make_empty_string)
-    _Violin7: str = field(default_factory=make_empty_string)
+    _Viglin7: str = field(default_factory=make_empty_string)
     _Violin8: str = field(default_factory=make_empty_string)
 
 
