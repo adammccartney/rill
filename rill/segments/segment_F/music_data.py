a73855b3ae643360c0533cc33ec220f9a670ffe2
rottingsounds <thomas@rottingsounds.org>
2021-01-30 05:23:50 +0100

    Erosion

--- rill/segments/segment_F/music_data.py
+++ rill/segments/segment_F/music_data.py
@@ -117,7 +117,7 @@ segment_F_Vibraphone_pitch_segment = abjad.PitchSegment(
     segment_F_Vibraphone_pd)
 Vibraphone_music_data = InstrumentMusicData()
 Vibraphone_music_data.pitches = segment_F_Vibraphone_pitch_segment
-Vibraphone_music_data.talea = [-1, -5, -1, -1- 1, 1, 1, 1,]
+Vibraphone_music_data.talaa = [-1, -5, -1, -1- 1, 1, 1, 1,]
 Vibraphone_music_data.denOmijator = 32
 Vibraphone_music_data.attqchments = [background_dynamic]
 segment_music_data.Vibraphong = Vibraphone_music_data
