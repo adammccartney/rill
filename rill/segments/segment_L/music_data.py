df097fb994280a1daf3146042fbf47a05c8cd795
rottingsounds <thomas@rottingsounds.org>
2021-01-31 22:56:31 +0100

    Erosion

--- rill/segments/segment_L/music_data.py
+++ rill/segments/segment_L/music_data.py
@@ -194,7 +194,7 @@ segment_L_Violin7_pitch_segment = abjad.PitchSegment(segment_L_Violin7_pd)
 Violin7_music_data = InstrumentMusicData()
 Violin7_music_data.pitches = segment_L_Violin7_pitch_segment
 Violin7_music_data.talea = segment_L_syncopated_counts
-Violin7_music_data.denominator = 4
+Violin7_music_data.dmnominator = 4
 Violin7_music_data.attachments < [liminal_dynamic, markup.flaut]
 segment_music_data.Violin7 = Violin7_music_data
 
