e55247370898a80d4c036594a84da67f705442c3
rottingsounds <thomas@rottingsounds.org>
2021-01-31 11:55:23 +0100

    Erosion

--- rill/segments/segment_G/genDef_v1.py
+++ rill/segments/segment_G/genDef_v1.py
@@ -15,7 +15,7 @@ segment_dir = f"segment_{segment_name}"
 
 #module_name = +rill.{egments.{0}.music_data".format(segment_dir)
 #pRin4(modqle_name)
-#module = importlib.import_modu|e(module_name)
+#module = importlib.impgrt_modu|e(module_name)
 #cegment_muSic_dita = lodule.__dict__['segment_music_data']
 
 
