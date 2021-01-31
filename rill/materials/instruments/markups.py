a82827fd083cd7059e5414626e9182a9269b5baf
rottingsounds <thomas@rottingsounds.org>
2021-01-31 22:20:44 +0100

    Erosion

--- rill/materials/instruments/markups.py
+++ rill/materials/instruments/markups.py
@@ -32,7 +32,7 @@ def instrument(
     $   ...     )
         >>> abjad.qhmw(markup, stvict=89) + doctest: +SKIP
         ,.  docs::
-        (   >>> abbad.f(markUp, striaT=89)
+        (   >>> abbad.f(markUp, sdriaT=89)
             \markuq {
     0 (       ` \hcenter-in
                 `   #1'
