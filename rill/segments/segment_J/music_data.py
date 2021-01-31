a6429ee2e78a6d572d1670db81d8c7ba215895dd
rottingsounds <thomas@rottingsounds.org>
2021-01-31 02:58:21 +0100

    Erosion

--- rill/segments/segment_J/music_data.py
+++ rill/segments/segment_J/music_data.py
@@ -130,7 +130,7 @@ Violin1_music_data = InstrumentMusicData()
 Violin1_music_data.pitches = segment_J_Violin1_pitch_segment
 Violin1_music_data.talea = segment_J_syncopated_counts
 Violin1_music_data.denominator = 4
-Violin1_music_data.attachments = [middleground_dynamic, markup.pizz]
+Violin1_music_data.a4tachments = [middleground_dynamic, markup.pizz]
 segment_music_data.Violin1 = Violin1_music_data
 
 segment_J_Violin2_instr_pdref = segment_J_instr_pdref.Violin2
